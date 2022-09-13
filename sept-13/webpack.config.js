d7ba6833b79ee523e1aa62524f8747d8d267045d
Anshika Jain <anshikajain@ingenuitygaming.com>
2022-09-13 13:53:07 +0530

    commit changes

--- sept-13/webpack.config.js
+++ sept-13/webpack.config.js
@@ -4,7 +4,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+
 const isProduction = process.env.NODE_ENV == "production";
 
 const stylesHandler = MiniCssExtractPlugin.loader;
@@ -12,7 +12,6 @@ const stylesHandler = MiniCssExtractPlugin.loader;
 const config = {
   entry: "./src/index.ts",
   output: {
-    clean: true,
     path: path.resolve(__dirname, "dist"),
   },
   devServer: {
@@ -25,11 +24,7 @@ const config = {
     }),
 
     new MiniCssExtractPlugin(),
-    new CopyPlugin({
-      patterns: [
-        { from: "src/assets", to: "assets" }
-      ],
-    }),
+
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
