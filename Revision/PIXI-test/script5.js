72789e75adc2c2581e66a7ad81293cfe2b18d1a1
Anshika Jain <anshikajain@ingenuitygaming.com>
2022-09-14 09:55:52 +0530

    commit changes

---
+++ Revision/PIXI-test/script5.js
@@ -0,0 +1,25 @@
+let app = new PIXI.Application({
+    width:800,
+    height:600,
+})
+document.body.appendChild(app.view);
+
+let sprite = new PIXI.Sprite.from('toy.png');
+
+sprite.anchor.set(0.5);
+sprite.x=app.screen.width/2;
+sprite.y=app.screen.height/2;
+sprite.width=200;
+sprite.height=200;
+
+sprite.interactive=true;
+sprite.buttonMode=true;
+sprite.on('pointerdown',onclick);
+app.stage.addChild(sprite);
+
+
+function onclick(){
+    sprite.scale.x*=1.25;
+    sprite.scale.y*=1.25;
+    
+}
